af4eff64fb9469d3804bfcb7c8c02d1a66d92e52
Steve Obaga <obaga4@gmail.com>
2019-03-28 09:37:12 +0300

    164829189-refactor: update code
    - update code to conform to current standards
    - implement path instead of url in url patterns
    
    [Finishes #164829189]

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -1,13 +1,17 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import (
     LoginAPIView, RegistrationAPIView, UserRetrieveUpdateAPIView
 )
 
+<<<<<<< HEAD
 app_name = 'authentication'
+=======
+app_name='authentication'
+>>>>>>> 164829189-refactor: update code
 
 urlpatterns = [
-    url(r'^user/?$', UserRetrieveUpdateAPIView.as_view()),
-    url(r'^users/?$', RegistrationAPIView.as_view()),
-    url(r'^users/login/?$', LoginAPIView.as_view()),
+    path('user/', UserRetrieveUpdateAPIView.as_view(), name='get'),
+    path('users/', RegistrationAPIView.as_view(), name='register'),
+    path('users/login/', LoginAPIView.as_view(), name='login'),
 ]
