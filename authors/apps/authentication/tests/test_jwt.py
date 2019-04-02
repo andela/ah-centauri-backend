f7ff3d7d88f751f7301aec91a9c33fb1dc9150ec
Vincent Omondi <ovincent7949@gmail.com>
2019-04-02 08:26:24 +0300

    164829200-feature-(fix): update validation
    - ensure special characters are not included in emails
    - add validation tests
    - refactor validation error messages to error_message module
    
    [Finishes 164829200]

--- authors/apps/authentication/tests/test_jwt.py
+++ authors/apps/authentication/tests/test_jwt.py
@@ -10,14 +10,15 @@ from authors.apps.authentication.backends import JWTAuthentication
 
 class JWTAuthenticationTest(TestCase):
     """ Test the JWT Authentication implementation """
+
     def setUp(self):
         self.user_data = {
             'user': {
                 'email': 'user1@mail.com',
                 'username': 'user1',
-                'password': 'password'
-                }
+                'password': 'sTr0ng%weR'
             }
+        }
         self.user = User.objects.create(
             username='user',
             email='user@mail.com',
@@ -35,7 +36,6 @@ class JWTAuthenticationTest(TestCase):
         )
         return res
 
-
     def test_user_gets_a_token_when_they_sign_up(self):
         """ Users should get a token when they successfully sign up """
         response = self.sign_up_user()
@@ -49,7 +49,7 @@ class JWTAuthenticationTest(TestCase):
             reverse('authentication:login'),
             self.user_data,
             format='json'
-            )
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('token', response.data)
 
@@ -69,12 +69,12 @@ class JWTAuthenticationTest(TestCase):
         """
         response = self.client.get(
             reverse('authentication:get')
-            )
+        )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual(
             response.data['detail'],
             'Authentication credentials were not provided.'
-            )
+        )
 
     def test_failure_if_user_provides_invalid_token(self):
         """ Test if an invalid token can be decoded """
@@ -85,7 +85,7 @@ class JWTAuthenticationTest(TestCase):
         self.assertEqual(
             response.data['detail'],
             'Invalid authentication. Could not decode token.'
-            )
+        )
 
     def test_failure_if_user_does_not_exist(self):
         """
@@ -98,7 +98,7 @@ class JWTAuthenticationTest(TestCase):
             username='test_user',
             email='test_user@mail.com',
             password='password'
-            )
+        )
         test_token = test_user.token
         test_user.delete()
         client = APIClient()
@@ -108,7 +108,7 @@ class JWTAuthenticationTest(TestCase):
         self.assertEqual(
             response.data['detail'],
             'No user matching this token was found.'
-            )
+        )
 
     def test_failure_because_user_is_inactive(self):
         """ Test if an inactive user can be authenticated """
@@ -116,7 +116,7 @@ class JWTAuthenticationTest(TestCase):
             username='inactive_one',
             email='inactive@mail.com',
             password='password'
-            )
+        )
         inactive_user.is_active = False
         inactive_user.save()
         headers = {'HTTP_AUTHORIZATION': f'Bearer {inactive_user.token}'}
@@ -125,7 +125,7 @@ class JWTAuthenticationTest(TestCase):
         self.assertEqual(
             response.data['detail'],
             'This user has been deactivated.'
-            )
+        )
 
     def test_authentication_failure_because_header_is_None(self):
         """ Test if authentication fails when a request has authorization
@@ -163,6 +163,7 @@ class JWTAuthenticationTest(TestCase):
         jwt_auth = JWTAuthentication()
         factory = APIRequestFactory()
         request = factory.get(reverse('authentication:get'))
-        request.META['HTTP_AUTHORIZATION'] = 'Token, {}'.format(self.user_token)
+        request.META['HTTP_AUTHORIZATION'] = 'Token, {}'.format(
+            self.user_token)
         response = jwt_auth.authenticate(request)
         self.assertEqual(response, None)
--- authors/apps/authentication/tests/test_serializers.py
+++ authors/apps/authentication/tests/test_serializers.py
@@ -5,7 +5,7 @@ from authors.apps.authentication.models import User
 from authors.apps.authentication.serializers import LoginSerializer
 from authors.apps.authentication.serializers import RegistrationSerializer
 from authors.apps.authentication.serializers import UserSerializer
-
+from authors.apps.authentication.error_messages import errors
 
 class RegistrationSerializerTests(TestCase):
     """
@@ -17,7 +17,7 @@ class RegistrationSerializerTests(TestCase):
         self.user = {
             "username": "user",
             "email": "user@mail.com",
-            "password": "password"
+            "password": "invAli3d#"
         }
 
     def test_register_method(self):
@@ -27,12 +27,129 @@ class RegistrationSerializerTests(TestCase):
         saved_user = serializer.save()
         self.assertIsInstance(saved_user, User)
 
+    def test_validation_fails_with_non_alphanumeric_password(self):
+        # setup
+        self.user.update({'password': 'password'})
+
+        # act
+        serializer = RegistrationSerializer(data=self.user)
+
+        # assert
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(
+            serializer.errors['password'][0],
+            errors['password']['weak_password'])
+
+    def test_validation_fails_if_missing_password(self):
+        # setup
+        self.user.pop('password')
+
+        # act
+        serializer = RegistrationSerializer(data=self.user)
+
+        # assert
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(
+            serializer.errors['password'][0], errors['password']['required'])
+
+    def test_password_min_length_validation(self):
+        # setup
+        self.user.update({'password': 'less'})
+
+        # act
+        serializer = RegistrationSerializer(data=self.user)
+
+        # assert
+        self.assertFalse(serializer.is_valid())
+        self.assertIn(errors['password']['min_length'],
+                      serializer.errors['password'])
+
+    def test_password_max_length_validation(self):
+        # setup
+        self.user.update({'password': 'l'*129})
+
+        # act
+        serializer = RegistrationSerializer(data=self.user)
+
+        # assert
+        self.assertFalse(serializer.is_valid())
+        self.assertIn(
+            errors['password']['max_length'],
+            serializer.errors['password'])
+
+    def test_validation_fails_if_blank_password(self):
+        # setup
+        self.user.update({'password': ''})
+
+        # act
+        serializer = RegistrationSerializer(data=self.user)
+
+        # assert
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(
+            serializer.errors['password'][0], errors['password']['required'])
+
+    def test_unique_username_validation(self):
+        # setup
+        # create user
+        User.objects.create_user(**self.user)
+
+        # act
+        # attempt to create another user with same username
+        self.user.update({'email': 'newunusedemail@emails.com'})
+        serializer = RegistrationSerializer(data=self.user)
+
+        # assert
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(
+            serializer.errors['username'][0], errors['username']['unique'])
+
+    def test_unique_email_validation(self):
+        # setup
+        # create user
+        User.objects.create_user(**self.user)
+
+        # act
+        # attempt to create another user with same email
+        self.user.update({'username': 'newusername'})
+        serializer = RegistrationSerializer(data=self.user)
+
+        # assert
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(
+            serializer.errors['email'][0], errors['email']['unique'])
+
+    def test_non_alphanumeric_emails_fail_validation(self):
+        # setup
+        self.user.update({'email': 'v123@e..com'})
+
+        # act
+        serializer = RegistrationSerializer(data=self.user)
+
+        # assert
+        self.assertFalse(serializer.is_valid())
+        self.assertIn(errors['email']['invalid'],
+                      serializer.errors['email'])
+
+    def test_invalid_username(self):
+        # setup
+        self.user.update({'username': 'ze-us'})
+
+        # act
+        serializer = RegistrationSerializer(data=self.user)
+
+        # assert
+        self.assertFalse(serializer.is_valid())
+        self.assertIn(errors['username']['invalid'],
+                      serializer.errors['username'])
+
 
 class LoginSerializerTests(TestCase):
     """
     Unit tests for the LoginSerializer class
     defined in our serializers.
     """
+
     def setUp(self):
         self.login_payload = {
             "email": "user@mail.com",
@@ -50,7 +167,7 @@ class LoginSerializerTests(TestCase):
         self.assertEqual(
             e.exception.detail[0],
             'A password is required to log in.'
-            )
+        )
 
     def test_login_no_email_validation(self):
         """ Test whether users can log in without an email. """
@@ -63,17 +180,35 @@ class LoginSerializerTests(TestCase):
         self.assertEqual(
             e.exception.detail[0],
             'An email address is required to log in.'
-            )
+        )
 
     def test_login_non_existent_user_validation(self):
-        """ Test whether users can log in without an email. """  
+        """ Test whether users can log in without an email. """
+
         serializer = LoginSerializer(data=self.login_payload)
         with self.assertRaises(ValidationError) as e:
             serializer.validate(self.login_payload)
         self.assertEqual(
             e.exception.detail[0],
             'A user with this email and password was not found.'
-            )
+        )
+
+    def test_login_existent_user_validation(self):
+        """ Test whether users can log in without an email. """
+        # register user
+        user = User.objects.create_user(
+            username="user",
+            email="user@mail.com",
+            password="password"
+        )
+        user.save()
+        serializer = LoginSerializer(data=self.login_payload)
+        user_data = serializer.validate(self.login_payload)
+        self.assertEqual({
+            "user": user,
+            "username": "user",
+            "email": "user@mail.com"
+        }, user_data)
 
     class UserSerializerTests(TestCase):
         """
