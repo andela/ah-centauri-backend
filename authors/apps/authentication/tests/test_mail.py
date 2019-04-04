b284a66871ed2beaea5581d515a968d3f66fea37
Steve Obaga <obaga4@gmail.com>
2019-04-04 16:37:33 +0300

    feature(email verification)-send user verification link via email
    - add functionality to send email upon user sign-up
    - add tests for email verification
    - add template for email
    
    [Delivers #164829201]

--- authors/apps/authentication/tests/test_mail.py
+++ authors/apps/authentication/tests/test_mail.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
+
 class EmailTest(TestCase):
     """ Test the Email verification implementation """
     def setUp(self):
@@ -16,7 +17,7 @@ class EmailTest(TestCase):
                 'password': 'password'
                 }
             }
-  
+ 
         self.client = APIClient()
 
     def sign_up_user(self):
@@ -37,23 +38,37 @@ class EmailTest(TestCase):
     def test_can_verify_mail(self):
         """Tests if the verification link works"""
         res = self.sign_up_user()
-        link = re.search(r'http:\/\/[a-zA-Z0-9]+\/[a-z]+\/[a-z]+-[a-z]+\/[-0-9a-z]+\/[A-z]+\/'\
-                         , mail.outbox[0].body)
+        link = re.search(
+            r'http:\/\/[a-zA-Z0-9]+\/[a-z]+\/[a-z]+-[a-z]+\/[-0-9a-z]+\/[A-z]+\/',
+            mail.outbox[0].body)
         url = link.group()
         res = self.client.get(url)
 
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.status_code, status.HTTP_202_ACCEPTED)
         self.assertEqual("Email successfully verified", res.data['message'])
 
-    def test_cannot_verify_with_invalid_link(self):
+    def test_cannot_verify_twice_with_same_link(self):
         """Tests that the verification link cannot be used twice"""
         res = self.sign_up_user()
-        link = re.search(r'http:\/\/[a-zA-Z0-9]+\/[a-z]+\/[a-z]+-[a-z]+\/[-0-9a-z]+\/[A-z]+\/'\
-                         , mail.outbox[0].body)
+        link = re.search(
+            r'http:\/\/[a-zA-Z0-9]+\/[a-z]+\/[a-z]+-[a-z]+\/[-0-9a-z]+\/[A-z]+\/',
+            mail.outbox[0].body)
         url = link.group()
-        res = self.client.get(url)
+        self.client.get(url)
         invalid_res = self.client.get(url)
-
+        
         self.assertEqual(invalid_res.status_code, status.HTTP_403_FORBIDDEN)
         self.assertEqual("Verification link has expired", invalid_res.data['message'])       
+
+    def test_cannot_verify_with_invalid_link(self):
+        """Tests that an invalid verification link cannot be used"""
+        res = self.sign_up_user()
+        link = re.search(
+            r'http:\/\/[a-zA-Z0-9]+\/[a-z]+\/[a-z]+-[a-z]+\/[-0-9a-z]+\/[A-z]+',
+            mail.outbox[0].body)
+        url = link.group()
+        invalid_url = url + 'k/'
+        res = self.client.get(invalid_url)
+
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
 
