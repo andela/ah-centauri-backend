d1773b1ab61988225b86a6f72198d253edb38531
Alfred Opon <alfredopon@Alfreds-MacBook-Pro.local>
2019-04-11 01:49:14 +0300

    164829211-Feature: Users should be able to follow each other
    -Add user follow endpoint
    -Add user unfollow endpoint
    -Add user retrieve following endpoint
    -Add user retrieve own following endpoint
    -Add proper permissions
    
    [Finishes #164829211]

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -17,7 +17,10 @@ urlpatterns = [
     path('users/', RegistrationAPIView.as_view(), name='register'),
     path('users/login/', LoginAPIView.as_view(), name='login'),
     path('users/social/', SocialOAuthAPIView.as_view(), name='social'),
-    path('users/password_reset/', PasswordResetAPIView.as_view(), name='password_reset'),
-    path('users/password_reset/<reset_token>/', SetPasswordAPIView.as_view(), name='password_change'),
-    path('verify-email/<token>/<uidb64>/', VerifyEmailView.as_view(), name='verify'),
+    path('users/password_reset/',
+         PasswordResetAPIView.as_view(), name='password_reset'),
+    path('users/password_reset/<reset_token>/',
+         SetPasswordAPIView.as_view(), name='password_change'),
+    path('verify-email/<token>/<uidb64>/',
+         VerifyEmailView.as_view(), name='verify'),
 ]
