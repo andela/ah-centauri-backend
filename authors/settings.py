0a97f31e0542ae127806e319290141038c5aa288
Daniel Kamar <danielkamar@Daniels-MacBook-Pro.local>
2019-05-09 10:10:17 +0300

    bug(cors-issues): hot-fix cors issues for frontend
    - enable CORS in Django REST framework by using a custom middleware
    - Only enable CORS for specified domains.
    
    [Delivers #165885656]

--- authors/settings.py
+++ authors/settings.py
@@ -207,10 +207,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
-CORS_ORIGIN_WHITELIST = (
-    '*'
-)
+# CORS configurations
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 # Tell Django about the custom `User` model we created. The string
 # `authentication.User` tells Django we are referring to the `User` model in
@@ -252,7 +256,7 @@ SWAGGER_SETTINGS = {
             'type': 'apiKey',
             'name': 'Authorization',
             'description': 'Token based authentication. ie Bearer token',
-            'in': 'header'
+            'in': 'header' 
         }
     }
 }
